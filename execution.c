2cb5734ca8a5c6b7475971d334fb21f674409fae
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-12-07 17:51:28 +0000

    Uploading main and Execusion files.

---
+++ execution.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+/**
+ * child_process - create a chlid process.
+ * Return: Always 0.
+ */
+pid_t child_process(void)
+{
+	pid_t process;
+
+	process = fork();
+
+	if (process < 0)
+	{
+		perror("Fork failed");
+		return (-1);
+	}
+	else
+		return (process);
+}
+/**
+ * _excev - execution program
+ * @token: ** pointer recived arguments.
+ * Return: Always 0.
+ */
+int _excev(char **token)
+{
+	int i = 0, j = 0;
+	char *argv[1024];
+
+	argv[0] = check_path(token[0]);
+	for (i = 1, j = 0; token[j]; i++, j++)
+	{
+		argv[i] = token[i];
+	}
+	if (execve(argv[0], argv, NULL) == -1)
+	{
+		perror("Error:");
+	}
+	return (0);
+}
+/**
+ * ctrol - controla el comando ctrl+c
+ */
+void ctrol(void)
+{
+	signal(SIGINT, ctrol);
+	write(STDOUT_FILENO, "\n:D : ", 6);
+	fflush(stdout);
+}
---
+++ main.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+/**
+ * main - execute the program.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char  *line;
+	size_t lineSize = 0;
+	int input = 0;
+	pid_t idenprocess;
+
+	signal(SIGINT, ctrol);
+	while (1)
+	{
+		prompt();
+		input = getline(&line, &lineSize, stdin); /
+		line[input - 1] = '\0';
+		if (_strcmp(line, "exit") == 0)
+		{
+			write(STDOUT_FILENO, "logout\n", 7);
+			break;
+		}
+		if (_strcmp(line, "env") == 0)
+			get_env();
+		if (input == -1)
+		{
+			write(STDOUT_FILENO, "logout\n", 7);
+			break;
+		}
+		else
+		{
+			idenprocess = child_process();
+			if (idenprocess == 0)
+			{
+				check_path(line);
+			}
+			if (idenprocess == 0)
+			{
+				tokenizer(line, " \t\n\"");
+			}
+			else
+				wait(NULL);
+		}
+	}
+	return (0);
+}
